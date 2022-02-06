167b5f8e0661063a8989994b7fb39435b342ae57
Olivier Bruchez <olivier@bruchez.org>
2022-02-06 10:27:44 +0100

    Data files are now downloaded using HTTPS, not S3

--- src/main/scala/org/bruchez/olivier/imdbstats/DownloadAll.scala
+++ src/main/scala/org/bruchez/olivier/imdbstats/DownloadAll.scala
@@ -1,26 +1,11 @@
 package org.bruchez.olivier.imdbstats
 
-import com.amazonaws.auth.{AWSCredentials, AWSCredentialsProvider, BasicAWSCredentials}
-import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.s3.model.GetObjectRequest
-import com.amazonaws.{AmazonClientException, AmazonServiceException}
-
-import java.io.{FileOutputStream, InputStream}
-
-// Quickly adapted from GetObject.java example code
+import java.io.File
+import java.net.URL
+import scala.sys.process._
 
 object DownloadAll {
-  private val bucketName = "imdb-datasets"
-
   def main(args: Array[String]): Unit = {
-    val credentialsProvider = new AWSCredentialsProvider {
-      override def getCredentials: AWSCredentials = new BasicAWSCredentials(args(0), args(1))
-
-      override def refresh(): Unit = ()
-    }
-
-    val s3Client = new AmazonS3Client(credentialsProvider)
-
     val filenames = Seq(
       "title.basics.tsv.gz",
       "title.crew.tsv.gz",
@@ -30,57 +15,9 @@ object DownloadAll {
       "name.basics.tsv.gz"
     )
 
-    filenames.foreach(download(s3Client, _))
+    filenames.foreach(download)
   }
 
-  private def download(s3Client: AmazonS3Client, filename: String): Unit = {
-    val key = s"documents/v1/current/$filename"
-
-    try {
-      // Note: It's necessary to set RequesterPays to true
-      val getObjectRequest =
-        new GetObjectRequest(bucketName, key).withRequesterPays(true)
-      println("Downloading object")
-      val s3object = s3Client.getObject(getObjectRequest)
-      println("Content-Type: " + s3object.getObjectMetadata.getContentType)
-      writeFile(s3object.getObjectContent, filename)
-    } catch {
-      case ase: AmazonServiceException =>
-        println(
-          "Caught an AmazonServiceException, which  means your request made it to " +
-            "Amazon S3, but was rejected with an error response for some reason."
-        )
-        println("Error Message:    " + ase.getMessage)
-        println("HTTP Status Code: " + ase.getStatusCode)
-        println("AWS Error Code:   " + ase.getErrorCode)
-        println("Error Type:       " + ase.getErrorType)
-        println("Request ID:       " + ase.getRequestId)
-      case ace: AmazonClientException =>
-        println(
-          "Caught an AmazonClientException, which means the client encountered an internal error " +
-            "while trying to communicate with S3, such as not being able to access the network."
-        )
-        println("Error Message: " + ace.getMessage)
-    }
-  }
-
-  private def writeFile(input: InputStream, filename: String): Unit = {
-    val buf = new Array[Byte](1024 * 1024)
-    val out = new FileOutputStream(filename)
-    var eof = false
-
-    while (!eof) {
-      val count = input.read(buf)
-
-      if (count < 0) {
-        eof = true
-      } else {
-        if (Thread.interrupted) throw new InterruptedException
-        out.write(buf, 0, count)
-      }
-    }
-
-    out.close()
-    input.close()
-  }
+  private def download(filename: String): Unit =
+    (new URL(s"https://datasets.imdbws.com/$filename") #> new File(filename)).!!
 }
